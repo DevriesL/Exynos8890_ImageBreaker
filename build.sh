bddb3663e40bd18ad52e8b09c2ffdc7f29c77223
DevriesL <therkduan@gmail.com>
2016-04-25 23:14:17 +0800

    Makefile:Fix CROSS_COMPILE path & add build script

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+    export ARCH=arm64
+
+    export PATH=/home/devries/aarch64-linux-gnu-5.3:$PATH
+
+    export PATH=/home/devries/GitHub/mkbootimg_tools:$PATH
+
+    export CROSS_COMPILE=/home/devries/aarch64-linux-gnu-5.3/bin/aarch64-
+
+    make exynos8890-hero2lte_defconfig
+
+    make -j7 2>&1 |tee build.txt
+
+mkbootimg \
+      --kernel arch/arm64/boot/Image \
+      --ramdisk ramdisk.packed \
+      --base 0x10000000 \
+      --pagesize 2048 \
+      --dt dt.img \
+      --ramdisk_offset 0x01000000 \
+      --tags_offset 0x00000100 \
+      --output boot.img
+
+tar -cvf boot.tar boot.img
