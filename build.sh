61be1e3f74d3abea452dabfae339ae8b2fb354ad
DevriesL <therkduan@gmail.com>
2016-07-25 22:55:33 +0800

    build: support S7 Flat
    Signed-off-by: DevriesL <therkduan@gmail.com>

--- build.sh
+++ build.sh
@@ -1,31 +1,85 @@
 #!/bin/bash
 
-    export ARCH=arm64
+DEVICE=$1
 
-    export PATH=/home/devries/aarch64-linux-gnu-5.3:$PATH
+make-config()
+{
+	if [ "$DEVICE" = "flat" ]; then
+		echo "Make S7 Flat defconfig."
+		make exynos8890-herolte_defconfig
+	elif [ "$DEVICE" = "edge" ]; then
+		echo "Make S7 Edge defconfig."
+		make exynos8890-hero2lte_defconfig
+	fi
+}
 
-    export PATH=/home/devries/GitHub/mkbootimg_tools:$PATH
+make-dtb()
+{
+	if [ "$DEVICE" = "flat" ]; then
+		echo "Make S7 Flat DeviceTree image."
+		tools/dtbtool -o dt-flat.img -s 2048 -p scripts/dtc/ arch/arm64/boot/dts/
+	elif [ "$DEVICE" = "edge" ]; then
+		echo "Make S7 Edge DeviceTree image."
+		tools/dtbtool -o dt.img -s 2048 -p scripts/dtc/ arch/arm64/boot/dts/
+	fi
+}
 
-    export CROSS_COMPILE=/home/devries/aarch64-linux-gnu-5.3/bin/aarch64-
+make-boot()
+{
+	if [ "$DEVICE" = "flat" ]; then
+		echo "Make S7 Flat Boot image."
+		mkbootimg \
+			--kernel arch/arm64/boot/Image \
+			--ramdisk ramdisk.packed \
+			--base 0x10000000 \
+			--pagesize 2048 \
+			--dt dt-flat.img \
+			--ramdisk_offset 0x01000000 \
+			--tags_offset 0x00000100 \
+			--output boot-flat.img
+		echo -n "SEANDROIDENFORCE" >> boot-flat.img
+		tar -cvf boot-flat.tar boot-flat.img
+	elif [ "$DEVICE" = "edge" ]; then
+		echo "Make S7 Edge Boot image."
+		mkbootimg \
+			--kernel arch/arm64/boot/Image \
+			--ramdisk ramdisk.packed \
+			--base 0x10000000 \
+			--pagesize 2048 \
+			--dt dt.img \
+			--ramdisk_offset 0x01000000 \
+			--tags_offset 0x00000100 \
+			--output boot.img
+		echo -n "SEANDROIDENFORCE" >> boot.img
+		tar -cvf boot.tar boot.img
+	fi
+}
 
-    make exynos8890-hero2lte_defconfig
+if [ ! -n "$1" ]; then
+	echo "No argument, Select S7 Edge as default."
+	DEVICE="edge"
+fi
 
-    make -j7 2>&1 |tee build.txt
+if [ "$DEVICE" = "flat" ] || [ "$DEVICE" = "edge" ]; then
+	export ARCH=arm64
 
-    make dtbs
+	export PATH=/home/devries/aarch64-linux-gnu-5.3:$PATH
 
-    tools/dtbtool -o dt.img -s 2048 -p scripts/dtc/ arch/arm64/boot/dts/
+	export PATH=/home/devries/GitHub/mkbootimg_tools:$PATH
 
-mkbootimg \
-      --kernel arch/arm64/boot/Image \
-      --ramdisk ramdisk.packed \
-      --base 0x10000000 \
-      --pagesize 2048 \
-      --dt dt.img \
-      --ramdisk_offset 0x01000000 \
-      --tags_offset 0x00000100 \
-      --output boot.img
+	export CROSS_COMPILE=/home/devries/aarch64-linux-gnu-5.3/bin/aarch64-
 
-echo -n "SEANDROIDENFORCE" >> boot.img
+	make-config
 
-tar -cvf boot.tar boot.img
+	make -j7 2>&1 |tee build.txt
+
+	rm arch/arm64/boot/dts/*.dtb
+
+	make dtbs
+
+	make-dtb
+
+	make-boot
+else
+	echo "Invalid argument, Input 'edge' or 'flat'."
+fi
